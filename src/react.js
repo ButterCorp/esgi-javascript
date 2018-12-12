ee89b1b3364a10175347120bdc97bb2576c07ea4
Younes DIOURI <younes.diouri@valeo.com>
2018-12-12 16:49:10 +0100

    refactor

--- src/react.js
+++ src/react.js
@@ -1,20 +1,11 @@
 (() => {
   function anElement(element, children) {
     if (isClass(element)) {
-      const component = new element();
-      return component.render();
-    } else if (typeof(element) === 'function') {
+      return handleClass(element);
+    } else if (isStateLessComponent(element)) {
       return element();
     } else {
-      const anElement = document.createElement(element);
-      children.forEach(child => {
-        if (typeof(child) === 'object') {
-          anElement.appendChild(child);
-        } else {
-          anElement.innerHTML += child;
-        }
-      });
-      return anElement;
+      return handleHtmlElement(element, children);
     }
   }
 
@@ -22,9 +13,21 @@
     return anElement(el, children);
   }
 
-  function isClass(func) {
-    return typeof func === 'function'
-      && /^class\s/.test(Function.prototype.toString.call(func));
+  function handleClass(clazz) {
+    const component = new clazz();
+    return component.render();
+  }
+
+  function handleHtmlElement(element, children) {
+    const anElement = document.createElement(element);
+    children.forEach(child => {
+      if (typeof(child) === 'object') {
+        anElement.appendChild(child);
+      } else {
+        anElement.innerHTML += child;
+      }
+    });
+    return anElement;
   }
 
   window.React = {
