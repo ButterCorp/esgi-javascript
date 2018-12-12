6b0f853111af872dd978d9e0c0f8163249396b13
Younes DIOURI <younes.diouri@valeo.com>
2018-12-12 16:46:23 +0100

    class & rendering management

--- src/app.js
+++ src/app.js
@@ -1,20 +1,8 @@
-const Hello = function () {
-    return React.createElement('div', null, `Hello World`);
-};
-const helloWorld = React.createElement(Hello, null, null);
-const helloWorld2 = React.createElement(Hello, null, null);
-const regularDiv = React.createElement('div', null, `I'm just a regular div`);
+class Hello {
+    render() {
+        return React.createElement('div', null, `Hello World`);
+    }
+}
 
-const parent = React.createElement('div', null,
-        helloWorld,
-        helloWorld2,
-        regularDiv,
-        ` I'm just a text`
-);
-// We expect to see:
-// -----------------
-// Hello World
-// Hello World
-// I'm just a regular div
-// I'm just a text
-ReactDOM.render(parent, document.getElementById('root'));
+const helloWorld = React.createElement(Hello, null, null);
+ReactDOM.render(helloWorld, document.getElementById('root'));
